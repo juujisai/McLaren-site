01928599a6e3b74e86a3b9d0e94ece0ee68168a4
Bartosz <bartosz.cylwik94@gmail.com>
2020-09-09 16:49:11 +0200

    add new articles to articleData.js

--- src/data/articleData.js
+++ src/data/articleData.js
@@ -1,4 +1,8 @@
-import article1 from '../images/articles/article1.jpg'
+import article1 from '../images/articles/article1.jpg';
+import article2 from '../images/articles/article2.jpg';
+import article3 from '../images/articles/article3.jpg';
+import article4 from '../images/articles/article4.jpg';
+import article5 from '../images/articles/article5.jpg';
 
 
 
@@ -6,15 +10,44 @@ import article1 from '../images/articles/article1.jpg'
 export default [
   {
     id: '000001',
+    title: '“These high-speed, medium-speed corners test the physical attributes of the driver to the limit."',
+    description: "As we prepare for this weekend's #TuscanGP, check out our quick introduction to Mugello Circuit. ",
+    fullText: [`Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, `  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`],
+    originalMclarenSite: 'https://www.mclaren.com/racing/inside-the-mtc/tuscan-grand-prix-handbook/',
+    img: article1,
+  },
+  {
+    id: '000002',
     title: 'Scrappy Fridays, Super Saturdays and Champagne Sundays.',
     description: 'Here are 8 things you might have missed from a dramatic #ItalianGP weekend',
     fullText: [`Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, `  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`],
     originalMclarenSite: 'https://www.mclaren.com/racing/2020/italian-grand-prix/things-you-missed/',
-    img: article1,
-
+    img: article2,
   },
-
-
+  {
+    id: '000003',
+    title: '"It was a decent Friday and the pace seemed encouraging."',
+    description: 'Read more from the team in our #ItalianGP Friday report.',
+    fullText: [`Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, `  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`],
+    originalMclarenSite: 'https://www.mclaren.com/racing/2020/italian-grand-prix/2020-italian-grand-prix-free-practice/',
+    img: article3,
+  },
+  {
+    id: '000004',
+    title: "It's time to put our Spa experience under the spotlight.",
+    description: 'Here are 7️ things you might have missed from the #BelgianGP weekend',
+    fullText: [`Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, `  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`],
+    originalMclarenSite: 'https://www.mclaren.com/racing/2020/belgian-grand-prix/things-you-missed/',
+    img: article4,
+  },
+  {
+    id: '000005',
+    title: "2020 BELGIAN GRAND PRIX – FREE PRACTICE",
+    description: '"We have a good baseline both in low fuel and high fuel"',
+    fullText: [`Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`, `  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo, error laborum eveniet dolores amet accusantium similique beatae aliquid nobis maxime perferendis optio. Voluptatum reiciendis corporis nobis assumenda iusto sequi.`],
+    originalMclarenSite: 'https://www.mclaren.com/racing/2020/belgian-grand-prix/2020-belgian-grand-prix-free-practice/',
+    img: article5,
+  },
 ]
 
 
