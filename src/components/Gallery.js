1bd7b196b7d77121a7ed42449c64d8a29cb90f2e
Bartosz <bartosz.cylwik94@gmail.com>
2020-09-09 16:24:47 +0200

    change styles for gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -11,8 +11,12 @@ const Gallery = ({ gallery }) => {
   }
 
   return (
-    <div className={`gallery-picture ${isClicked ? `zoom-gallery-pic` : 'null'}`} >
+    <div className={`gallery-picture ${isClicked ? `zoom-gallery-pic` : ''}`} >
       <img className={`gallery-img ${orientation}`} onClick={handleClick} src={img} alt={title} />
+      <div className={isClicked ? 'shadow shadow-off' : 'shadow shadow-on'} >
+
+      </div>
+
       <div className="picture-desc">
         <h2 className='picture-title'>{title}</h2>
         {!isClicked && <p>{description}</p>}
