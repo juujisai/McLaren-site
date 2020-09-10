718271b4393da8b71112b1231930069a72f993ef
Bartosz <bartosz.cylwik94@gmail.com>
2020-09-10 18:39:25 +0200

    add driverData

---
+++ src/data/driverData.js
@@ -0,0 +1,82 @@
+import lando1 from '../images/driver/lando1.jpg'
+import lando2 from '../images/driver/lando2.jpg'
+import landoGif from '../images/driver/lando_gif.gif'
+
+import carlos1 from '../images/driver/carlos1.jpg'
+import carlos2 from '../images/driver/carlos2.jpg'
+import carlosGif from '../images/driver/carlos_gif.gif'
+
+
+export default [
+  {
+    name: 'Lando',
+    secondName: 'Norris',
+    image: [lando1, lando2],
+    number: '4',
+    f1Debut: 'australia 2019',
+    birth: '13 november 1999',
+    homeCity: 'bristol / uk',
+    video: landoGif,
+    info: [`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum aut, beatae maxime architecto aliquid nulla doloremque ducimus deleniti expedita deserunt?`, `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas excepturi ipsam eum dolorum quod quae repellat nulla laudantium, voluptatem magnam praesentium deserunt aspernatur tempore molestias!`, `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, omnis?`, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eum sed recusandae nulla amet, earum vitae voluptatem minima aliquid omnis impedit eaque hic ad nisi.`, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae incidunt ad laboriosam eos, vitae commodi hic deserunt harum quis ducimus nesciunt quo qui sapiente.`],
+    twitter: `<a class="twitter-timeline" data-width="400" data-theme="light" href="https://twitter.com/LandoNorris?ref_src=twsrc%5Etfw">Tweets by LandoNorris</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`,
+    socialMedia: [
+      {
+        name: 'website',
+        path: 'https://landonorris.com/',
+      },
+      {
+        name: 'facebook',
+        path: 'https://www.facebook.com/landonorris1/',
+      },
+      {
+        name: 'twitter',
+        path: 'https://twitter.com/landonorris',
+      },
+      {
+        name: 'instagram',
+        path: 'https://www.instagram.com/landonorris/',
+      },
+      {
+        name: 'youtube',
+        path: 'https://www.youtube.com/landonorris04',
+      },
+      {
+        name: 'twitch',
+        path: 'https://www.twitch.tv/landonorris',
+      },
+    ],
+
+  },
+
+  {
+    name: 'Carlos',
+    secondName: 'Sainz',
+    image: [carlos1, carlos2],
+    number: '55',
+    f1Debut: 'australia 2015',
+    birth: '1 september 1994',
+    homeCity: 'madrid / spain',
+    video: carlosGif,
+    info: [`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum aut, beatae maxime architecto aliquid nulla doloremque ducimus deleniti expedita deserunt?`, `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas excepturi ipsam eum dolorum quod quae repellat nulla laudantium, voluptatem magnam praesentium deserunt aspernatur tempore molestias!`, `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, omnis?`, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eum sed recusandae nulla amet, earum vitae voluptatem minima aliquid omnis impedit eaque hic ad nisi.`, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae incidunt ad laboriosam eos, vitae commodi hic deserunt harum quis ducimus nesciunt quo qui sapiente.`],
+    twitter: `<a class="twitter-timeline" data-width="400" href="https://twitter.com/Carlossainz55?ref_src=twsrc%5Etfw">Tweets by Carlossainz55</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`,
+    socialMedia: [
+      {
+        name: 'website',
+        path: 'https://www.mclaren.com/racing/team/carlos-sainz/',
+      },
+      {
+        name: 'twitter',
+        path: 'https://twitter.com/Carlossainz55',
+      },
+      {
+        name: 'instagram',
+        path: 'https://www.instagram.com/carlossainz55/',
+      },
+      {
+        name: 'youtube',
+        path: 'https://www.youtube.com/carlossainz',
+      },
+    ],
+
+  },
+]
