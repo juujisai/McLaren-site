ad2efd317e50cda36b31f665b195552a86399887
Bartosz <bartosz.cylwik94@gmail.com>
2020-09-11 15:31:23 +0200

    add styles to DriverShortInfo

--- src/components/DriverShortInfo.js
+++ src/components/DriverShortInfo.js
@@ -8,7 +8,8 @@ const DriverShortInfo = ({ info }) => {
 
 
   return (
-    <div>
+    <div className='driver-short'>
+
       <div className="driver-data-short">
         <h3 className={`driver-data-name driver-${name}`}><span className='short-name'>{name}</span> <span className='short-second-name'>{secondName}</span></h3>
         <h4 className='driver-number'>{number}</h4>
@@ -16,14 +17,17 @@ const DriverShortInfo = ({ info }) => {
           <img className='full-img' src={image[0]} alt={name} />
         </div>
       </div>
+
+      <div className="image-gif">
+        <img className='full-img' src={video} alt={name} />
+        <Link className='btn-goto-driver' to={`/drivers/${name}${secondName}`}>view profile</Link>
+      </div>
+
       <div className="image-img2">
         <img className='full-img' src={image[1]} alt={name} />
       </div>
-      <div className="image-gif">
-        <img className='full-img' src={video} alt={name} />
-      </div>
 
-      <Link to={`/drivers/${name}${secondName}`}>{name} {secondName}</Link>
+
 
     </div>
   );
