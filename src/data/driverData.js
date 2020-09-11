7fc721b6644a8187a7cda0571c3752f765b330d8
Bartosz <bartosz.cylwik94@gmail.com>
2020-09-11 17:36:52 +0200

    add data to DriverPage

--- src/data/driverData.js
+++ src/data/driverData.js
@@ -1,10 +1,11 @@
 import lando1 from '../images/driver/lando1.jpg'
 import lando2 from '../images/driver/lando2.jpg'
 import landoGif from '../images/driver/lando_gif.gif'
-
+import landoPng from '../images/lando_cropp.png'
 import carlos1 from '../images/driver/carlos1.jpg'
 import carlos2 from '../images/driver/carlos2.jpg'
 import carlosGif from '../images/driver/carlos_gif.gif'
+import carlosPng from '../images/carlos_cropp.png'
 
 
 export default [
@@ -12,13 +13,13 @@ export default [
     name: 'lando',
     secondName: 'norris',
     image: [lando1, lando2],
+    imagePng: landoPng,
     number: '4',
     f1Debut: 'australia 2019',
     birth: '13 november 1999',
     homeCity: 'bristol / uk',
     video: landoGif,
     info: [`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum aut, beatae maxime architecto aliquid nulla doloremque ducimus deleniti expedita deserunt?`, `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas excepturi ipsam eum dolorum quod quae repellat nulla laudantium, voluptatem magnam praesentium deserunt aspernatur tempore molestias!`, `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, omnis?`, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eum sed recusandae nulla amet, earum vitae voluptatem minima aliquid omnis impedit eaque hic ad nisi.`, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae incidunt ad laboriosam eos, vitae commodi hic deserunt harum quis ducimus nesciunt quo qui sapiente.`],
-    twitter: `<a class="twitter-timeline" data-width="400" data-theme="light" href="https://twitter.com/LandoNorris?ref_src=twsrc%5Etfw">Tweets by LandoNorris</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`,
     socialMedia: [
       {
         name: 'website',
@@ -52,13 +53,13 @@ export default [
     name: 'carlos',
     secondName: 'sainz',
     image: [carlos1, carlos2],
+    imagePng: carlosPng,
     number: '55',
     f1Debut: 'australia 2015',
     birth: '1 september 1994',
     homeCity: 'madrid / spain',
     video: carlosGif,
     info: [`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum aut, beatae maxime architecto aliquid nulla doloremque ducimus deleniti expedita deserunt?`, `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas excepturi ipsam eum dolorum quod quae repellat nulla laudantium, voluptatem magnam praesentium deserunt aspernatur tempore molestias!`, `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, omnis?`, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eum sed recusandae nulla amet, earum vitae voluptatem minima aliquid omnis impedit eaque hic ad nisi.`, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae incidunt ad laboriosam eos, vitae commodi hic deserunt harum quis ducimus nesciunt quo qui sapiente.`],
-    twitter: `<a class="twitter-timeline" data-width="400" href="https://twitter.com/Carlossainz55?ref_src=twsrc%5Etfw">Tweets by Carlossainz55</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`,
     socialMedia: [
       {
         name: 'website',
--- src/pages/DriverPage.js
+++ src/pages/DriverPage.js
@@ -1,18 +1,91 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom'
 
+import { DriverContext } from '../context/DriverContext'
+
+import Loader from '../components/Loader'
+
+
 const DriverPage = () => {
 
   const { name } = useParams()
 
-  return (
-    <div>Hello from {name}
-      <Link to="/drivers">Go back to drivers</Link>
+  const { driversInfo, loading } = React.useContext(DriverContext)
 
-    </div>
-  );
+  console.log(driversInfo)
+
+  const driver = driversInfo.find(one => `${one.name}${one.secondName}` === name)
+  console.log(driver)
+
+  if (loading || typeof driver === 'undefined') {
+    return <Loader />
+  }
+
+  if (typeof driver !== 'undefined') {
+    const { name, secondName, image, imagePng, number, f1Debut, birth, homeCity, video, info, socialMedia } = driver
+
+
+    let randomId1 = Math.floor(Math.random() * socialMedia.length) - 1
+    let randomId2 = Math.floor(Math.random() * socialMedia.length) - 1
+
+    const paragraphs = info.map((par, id) => {
+      return (
+        <div key={id} className='text-cont-flex'>
+          <p >{par}</p>
+          {randomId1 === id && <div className="photos"><img src={image[0]} alt={`${name} ${secondName}`} className="full-img" /></div>}
+          {randomId2 === id && <div className="photos"><img src={image[1]} alt={`${name} ${secondName}`} className="full-img" /></div>}
+        </div>
+      )
+    })
+
+    const socials = socialMedia.map((social, id) => (
+      <div key={id}>
+        {social.name}
+      </div>
+    )
+    )
+
+
+    return (
+      <div className="driver-page">
+
+        <div className="driver-full-info">
+          <div className="img-cont">
+            <img className={`full-img`} src={imagePng} alt={`${name} ${secondName}`} />
+          </div>
+
+          <h2 className="name-full">{name} {secondName}</h2>
+          <h3>About</h3>
+          <p>Number: <span>{number}</span></p>
+          <p>Home City: <span>{homeCity}</span></p>
+          <p>Birth: <span>{birth}</span></p>
+          <p>F1 debut: <span>{f1Debut}</span></p>
+
+        </div>
+        <div className="video">
+          <img className='full-img' src={video} alt={`${name} ${secondName}`} />
+        </div>
+
+        {/* <div className="photos">
+          <img src={image[0]} alt={`${name} ${secondName}`} className="full-img" />
+          <img src={image[1]} alt={`${name} ${secondName}`} className="full-img" />
+        </div> */}
+
+        <div className="info-page">
+          {paragraphs}
+        </div>
+
+        <div className="social-media">
+          {socials}
+        </div>
+
+        <div className='link-back'>
+          <Link className={'btn-link'} to="/drivers">Go back to drivers</Link>
+        </div>
+      </div>
+    );
+  }
+
 }
-
-
 
 export default DriverPage;
