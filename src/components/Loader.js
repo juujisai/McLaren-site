9d31e9cd78f24da8836928437dfb40e6a601d243
Bartosz <bartosz.cylwik94@gmail.com>
2020-09-15 21:03:34 +0200

    change in loading animation for Loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
-import { GiArrowDunk as Arrow } from 'react-icons/gi'
+import load from '../images/tire.png'
+// import { GiArrowDunk as Arrow } from 'react-icons/gi'
 
 const Loader = () => {
+
   return (
     <div className='load'>
-      <div className="loading-arrow"><Arrow /></div>
+      <div className="loading-arrow">
+        {/* <Arrow /> */}
+        <img src={load} alt="loading" />
+
+      </div>
       <p>Loading ...</p>
     </div>
   );
