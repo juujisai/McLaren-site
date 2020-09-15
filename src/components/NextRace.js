47c8a62f67ce9b03f7b5927846c3342915e7d0a1
Bartosz <bartosz.cylwik94@gmail.com>
2020-09-15 16:34:46 +0200

    add and style working countdown to next race

--- src/components/NextRace.js
+++ src/components/NextRace.js
@@ -23,6 +23,13 @@ class NextRace extends React.Component {
     lastRace: '',
     nextRace: '',
 
+    timeToNext: {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }
+
   }
 
 
@@ -32,7 +39,7 @@ class NextRace extends React.Component {
     const checkSchedule1 = schedule.filter(race => race.month.includes(this.state.todaysDate.month))
 
     if (checkSchedule1.length !== 0) {
-      checkSchedule1[checkSchedule1.length - 1].month.includes('/') ? checkSchedule1.pop() : console.log('left alone')
+      checkSchedule1[checkSchedule1.length - 1].month.includes('/') && checkSchedule1.pop()
 
       let nextRace = checkSchedule1.filter(race => Number(race.dayEnd) >= this.state.todaysDate.day)
 
@@ -58,7 +65,58 @@ class NextRace extends React.Component {
     }
   }
 
+  timeToNext() {
 
+    const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+
+    let { year, month, day, hour, minute, second } = this.state.todaysDate
+
+    second = Number(second)
+
+    month = months.findIndex(id => id === month) + 1
+
+    const todayDate = new Date(year, month, day, hour, minute, second)
+
+
+    let monthOfRace = this.state.nextRace.month
+
+    let dayOfRace = this.state.nextRace.sessions[4].date
+    let hourOfRace = this.state.nextRace.sessions[4].time
+
+    // to split month of the race that end and start on different months
+    if (monthOfRace.includes('/')) {
+      let indexToDelete = (monthOfRace.indexOf('/')) + 1
+      monthOfRace = monthOfRace.split('').splice(indexToDelete, monthOfRace.length - indexToDelete)
+      monthOfRace.shift()
+      monthOfRace = monthOfRace.join('')
+    }
+
+    monthOfRace = months.findIndex(id => id === monthOfRace) + 1
+
+    const raceDate = new Date(year, monthOfRace, dayOfRace, hourOfRace, 10, 0)
+
+
+
+
+    let timeDelta = raceDate.getTime() - todayDate.getTime()
+    timeDelta = new Date(timeDelta)
+
+
+    let daysLeft = hour >= hourOfRace ? timeDelta.getDate() - 1 : timeDelta.getDate()
+
+    const timeToNext = {
+      days: daysLeft,
+      hours: timeDelta.getHours(),
+      minutes: timeDelta.getMinutes(),
+      seconds: timeDelta.getSeconds(),
+    }
+
+
+    this.setState({
+      timeToNext
+    })
+
+  }
 
 
 
@@ -66,18 +124,20 @@ class NextRace extends React.Component {
 
   componentDidMount() {
 
-
     this.idOfInterval = setInterval(() => {
       this.findNextRace()
+
+      this.state.nextRace !== '' && this.timeToNext()
+
 
       const newDate = new Date()
 
       const newCurrentDate = {
         year: newDate.getFullYear(),
         month: newDate.toLocaleString('en-EN', { month: 'long' }).toLowerCase(),
-        // month: 'august',
+        // month: 'september',
         day: newDate.getDate(),
-        // day: '1',
+        // day: '26',
         hour: newDate.getHours().toLocaleString().length === 1 ? `0${newDate.getHours()}` : newDate.getHours(),
         minute: newDate.getMinutes().toLocaleString().length === 1 ? `0${newDate.getMinutes()}` : newDate.getMinutes(),
         second: newDate.getSeconds().toLocaleString().length === 1 ? `0${newDate.getSeconds()}` : newDate.getSeconds()
@@ -106,13 +166,17 @@ class NextRace extends React.Component {
 
     const dateCont = (
       <div>
-        Today is:
-        <p>{todaysDate.year}</p>
-        <p>{todaysDate.month}</p>
-        <p>{todaysDate.day}</p>
-        <p>{todaysDate.hour}</p>
-        <p>{todaysDate.minute}</p>
-        <p>{todaysDate.second}</p>
+        <h3>Today is</h3>
+        <div className="date-callendar">
+          <p className='date-today-year'>{todaysDate.year}</p>
+          <p className='date-today-month'>{todaysDate.month}</p>
+          <p className='date-today-month'>{todaysDate.day}</p>
+        </div>
+        <div className="date-time">
+          <p className='time-hour'>{todaysDate.hour} :</p>
+          <p className='time-minute'>{todaysDate.minute} :</p>
+          <p className='time-second'>{todaysDate.second}</p>
+        </div>
       </div>
     )
 
@@ -127,21 +191,31 @@ class NextRace extends React.Component {
     const raceTime = `${race.time}:10`
 
 
+    let { days, hours, minutes, seconds } = this.state.timeToNext
+    let array = [hours, minutes, seconds]
+
+    array.forEach(time => time.toString().length === 1 ? time === `0${time}` : time === `${time}`)
 
     return (
-      <div>
+      <div className='next-race'>
 
-        {typeof todaysDate.year !== 'undefined' && dateCont}
+        <div className="date-today">
+          {typeof todaysDate.year !== 'undefined' && dateCont}
+        </div>
 
-        <p>
-          Next race {country} {this.state.nextRace.stats[0].name}
-          <br />
-          date: {raceDate}-{month}-{raceTime}
+        <div className="next-race-info">
 
-        </p>
+          <h3>Next race: {country}</h3>
+          <h4>track: {this.state.nextRace.stats[0].name}</h4>
+          <h4>date: {raceDate}-{month}-{raceTime}</h4>
+
+          <div className="time-to-next">
+            <p>{days > 0 && <span className="next-days">{days} days </span>}<span className="next-hours">{hours.toString().length === 1 ? `0${hours}` : `${hours}`}</span> : <span className="next-minutes">{minutes.toString().length === 1 ? `0${minutes}` : `${minutes}`}</span> : <span className="next-seconds">{seconds.toString().length === 1 ? `0${seconds}` : `${seconds}`}</span></p>
+          </div>
 
 
-        Hello from next race</div >
+        </div>
+      </div >
     );
   }
 }
